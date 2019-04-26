be39a42b9cfc34abaf5a003f78d83ce392fe63a0
Arifia Kasastra R <arka.progammer@gmail.com>
2019-04-27 06:09:57 +0700

    Add Navigation From WelcomeActivity DriverLoginRegisterActivity to Driver and Customer Login Register

--- app/src/main/java/com/example/cabapps/WelcomeActivity.java
+++ app/src/main/java/com/example/cabapps/WelcomeActivity.java
@@ -1,13 +1,38 @@
 package com.example.cabapps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button gotoDriver;
+    Button gotoCustomer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        gotoCustomer = (Button) findViewById(R.id.loginCustomerbtn);
+        gotoDriver = (Button) findViewById(R.id.loginDriverbtn);
+
+        gotoCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent CustomerIntent = new Intent(WelcomeActivity.this, CustomerLoginRegisterActivity.class);
+                startActivity(CustomerIntent);
+            }
+        });
+
+        gotoDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent DriverIntent = new Intent(WelcomeActivity.this, DriverLoginRegisterActivity.class);
+                startActivity(DriverIntent);
+            }
+        });
     }
 }
